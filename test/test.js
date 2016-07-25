5e4de84cd5097fbd45ce60d98d2ca1c161a3e0ce
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-07-25 16:28:41 +0900

    Add tests and new words from Al-jazeera

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ setTimeout(function(){
     showPopup(e, TransOver.formatTranslation(response.translation));
   });
   chrome.extension.sendRequest({handler: 'translate', word: "المنظمة"}, function(response) {
-    var e = { clientX: 0, clientY: 0 };
+    var e = { clientX: 0, clientY: 20 };
     showPopup(e, TransOver.formatTranslation(response.translation));
   });
   chrome.extension.sendRequest({handler: 'translate', word: "املاك"}, function(response) {
@@ -20,6 +20,35 @@ setTimeout(function(){
   chrome.extension.sendRequest({handler: 'translate', word: "اسمه"}, function(response) {
     var e = { clientX: 400, clientY: 850 };
     showPopup(e, TransOver.formatTranslation(response.translation));
+  });
+
+  $('#urlInput').keypress(function(event) {
+      console.log(event);
+      if (event.keyCode == 13) {
+          var url = event.target.value;
+          console.log(url);
+          $.get(url, function(data) {
+              //console.log(data);
+              var object = $($.parseHTML(data));
+              var text = object.text().trim();
+              var words = XRegExp.matchChain(text, [XRegExp("[\\p{ArabicWord}]+", "g")]);
+              var wordCount = words.length;
+              console.log([wordCount, words]);
+              var counter = 0;
+              var unknownWords = {};
+              words.forEach(function(word) {
+                chrome.extension.sendRequest({handler: 'translate', word: word}, function(response) {
+                    if(!(response.translation instanceof Array)) {
+                        unknownWords[word] = response;
+                    }
+                    counter++;
+                    if (counter == wordCount) {
+                        console.log(unknownWords);
+                    }
+                });
+              });
+          });
+      }
   });
 
 }, 500);
